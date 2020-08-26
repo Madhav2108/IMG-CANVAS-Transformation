58f5aac3d5f63db9157d5885c9448a9f43dbf23d
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-08-27 01:58:52 +0530

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,105 @@
 // ✏ ɯσɾƙιɳɠ σɳ Fυʅʅ ʂƚαƈƙ
 // ✏  || Pɾσԃυƈƚ ɱαɾƙҽƚιɳɠ ||
 // COntact For Make Projects
+
+import {
+    BufferGeometry,
+    Color,
+    DoubleSide,
+    Face3,
+    Geometry,
+    InstancedBufferAttribute,
+    InstancedMesh,
+    MathUtils,
+    MeshBasicMaterial,
+    Object3D,
+    PerspectiveCamera,
+    Scene,
+    TextureLoader,
+    Vector2,
+    Vector3,
+    WebGLRenderer
+}from 'https://unpkg.com/three@0.119.0/build/three.module.js';
+
+  function App() {
+    const conf = {
+      size: 10,
+      images: [
+        { src: '' },
+        { src: '' },
+        { src: '' },
+        { src: '' }
+      ]
+    };
+
+let renderer, scene, camera, cameraCtrl;
+  const screen = {
+    width: 0, height: 0,
+    wWidth: 0, wHeight: 0,
+    ratio: 0
+  };
+
+  const loader = new TextureLoader();
+  const textures = [];
+  let planes, plane1, plane2;
+  let progress = 0, targetProgress = 0;
+
+  const mouse = new Vector2();
+
+  init();
+
+  function init() {
+    renderer = new WebGLRenderer({ canvas: document.getElementById('canvas'), antialias: true });
+
+    camera = new PerspectiveCamera(50);
+    camera.position.z = 150;
+
+    updateSize();
+    window.addEventListener('resize', onResize);
+
+    Promise.all(conf.images.map(loadTexture)).then(responses => {
+      initScene();
+      initListeners();
+
+      gsap.fromTo(plane1.uProgress,
+        {
+          value: -2
+        },
+        {
+          value: 0,
+          duration: 2.5,
+          ease: Power4.easeOut
+        }
+      );
+
+      requestAnimationFrame(animate);
+    });
+  }
+
+  function initScene() {
+    scene = new Scene();
+    scene.background = new Color(0);
+
+    plane1 = new AnimatedPlane({
+      renderer, screen,
+      size: conf.size,
+      anim: 1,
+      texture: textures[0]
+    });
+
+    plane2 = new AnimatedPlane({
+      renderer, screen,
+      size: conf.size,
+      anim: 2,
+      texture: textures[1]
+    });
+
+    setPlanesProgress(0);
+
+    planes = new Object3D();
+    planes.add(plane1.o3d);
+    planes.add(plane2.o3d);
+    scene.add(planes);
+  }
+
+}
